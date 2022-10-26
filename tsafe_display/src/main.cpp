8aa3706a914ca171a5a04666b43dc07b7da999aa
Matt Murphy <matt@murphysys.com>
2022-10-25 19:13:42 -0700

    Beginnings of a streaming image protocol

--- tsafe_display/src/main.cpp
+++ tsafe_display/src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <BluetoothSerial.h>
-#include <TFT_eSPI.h> // Include the graphics library (this includes the sprite functions)
+#include <TFT_eSPI.h>
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
   #error Bluetooth is not enabled! Please run `make menuconfig` to enable it
@@ -10,14 +10,25 @@
   #error Serial Bluetooth not available or not enabled. It is only available for the ESP32 chip.
 #endif
 
-TFT_eSPI tft = TFT_eSPI(); // Create object "tft"
 
-BluetoothSerial SerialBT;
-#define BT_DISCOVER_TIME 10000
-esp_spp_sec_t sec_mask
-    = ESP_SPP_SEC_NONE; // or ESP_SPP_SEC_ENCRYPT|ESP_SPP_SEC_AUTHENTICATE to
-                        // request pincode confirmation
-esp_spp_role_t role = ESP_SPP_ROLE_SLAVE; // or ESP_SPP_ROLE_MASTER
+#define DWIDTH 135
+#define DHEIGHT 240
+
+bool connected;
+
+// Display driver
+TFT_eSPI tft       = TFT_eSPI();
+// Image data, with backbuffer
+TFT_eSprite sprite = TFT_eSprite(&tft);
+uint8_t* spriteImage;
+
+size_t spriteCursor;
+
+// Bluetooth driver
+BluetoothSerial btSerial;
+#define READBUF_SIZE 33000
+char readBuf[READBUF_SIZE];
+
 
 void
 setup()
@@ -27,29 +38,68 @@ setup()
 
   tft.fillScreen(TFT_RED);
 
-  Serial.begin(115200);
+  Serial.begin(9600);
+  Serial.println();
+  Serial.println("========== TSAFE ==========");
 
-  if (!SerialBT.begin("TSAFE", false)) {
+  if (!btSerial.begin("TSAFE", false)) {
     Serial.println("! Couldn't start BluetoothSerial");
     abort();
   }
+  Serial.println("? Bluetooth ready");
+  connected = false;
+
+  spriteImage = (uint8_t*) sprite.createSprite(DWIDTH, DHEIGHT);
+  if (!spriteImage) {
+    Serial.println("! Couldn't create sprite");
+    abort();
+  }
+  sprite.setColorDepth(8);
+  spriteCursor = 0;
+
+  // Hold TFT chip select down
+  tft.startWrite();
+
+  Serial.println("? Display ready");
 }
+
 
 void
 loop()
 {
-  if (SerialBT.connected()) {
-    tft.fillScreen(TFT_BLUE);
-  } else {
+  if (!btSerial.connected()) {
     tft.fillScreen(TFT_RED);
+    delay(100);
+    if (connected) {
+      Serial.println("? Bluetooth disconnected");
+      connected = false;
+    }
+    return;
+
+  } else if (!connected) {
+    Serial.println("? Bluetooth connected!");
+    tft.fillScreen(TFT_BLACK);
+    connected = true;
   }
 
-  if (Serial.available()) {
-    SerialBT.write(Serial.read());
-  }
-  if (SerialBT.available()) {
-    Serial.write(SerialBT.read());
+  if (!btSerial.available())
+    return;
+
+  size_t bytesRead = btSerial.readBytes(readBuf, READBUF_SIZE);
+  for (size_t i = 0; i < bytesRead; ++i) {
+    uint8_t byte = readBuf[i];
+
+    if (byte & 0b1000000) {
+      // Frame preamble
+      spriteCursor = 0;
+      sprite.fillSprite(TFT_BLACK);
+    } else {
+      // Frame data
+      if (spriteCursor < DWIDTH * DHEIGHT)
+        // TODO: sample color gradient instead
+        spriteImage[spriteCursor++] = byte;
+    }
   }
 
-  delay(20);
+  sprite.pushSprite(0, 0);
 }
